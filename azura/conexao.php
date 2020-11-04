044a346baa1a3552649ce436e9a1e872cca044ec
Gustavo Vaz <68481189+gustavovazweb@users.noreply.github.com>
2020-11-04 13:08:11 -0300

    Commit - Upload backend files

---
+++ azura/conexao.php
@@ -0,0 +1,23 @@
+<?php
+
+	class Conexao{
+
+		private $host = 'localhost';
+		private $dbname = 'azura';
+		private $user = 'root';
+		private $pass = '';
+
+		public function conectar(){
+
+			$conexao = new PDO(
+				"mysql:host=$this->host;dbname=$this->dbname",
+				"$this->user",
+				"$this->pass"				
+			);
+
+			return $conexao;
+		}
+	}
+
+
+?>
